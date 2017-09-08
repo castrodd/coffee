0161ac6792919e9ad8d84585ce75ca45d0da4d3e
Daniel Castro <castrodd@gmail.com>
2017-09-07 23:26:59 -0400

    UI design

--- scripts/main.js
+++ scripts/main.js
@@ -5,4 +5,17 @@
   var DataStore = App.DataStore;
   var myTruck = new Truck('Falcon', new DataStore());
   window.myTruck = myTruck;
+
 }) (window);
+
+var button = document.querySelector('.reset');
+
+var slider = new Slider('#strengthLevel', {
+formatter: function(value) {
+  return 'Current value: ' + value;
+}
+});
+
+button.addEventListener('click', function() {
+  slider.setValue(5);
+})
